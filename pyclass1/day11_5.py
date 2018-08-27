02c22a9cf4ecc48221d62ccbba868592ad1361b3
dnjstmdwo1234 <ghu970@hanmail.net>
2018-08-27 18:03:19 +0900

    sdlksadl

--- pyclass1/day11_5.py
+++ pyclass1/day11_5.py
@@ -1,8 +1,20 @@
 # 부트스트랩 페이지 연결하기  
 # 
 
+import csv
+import random
+ 
+f = open('ddareng.csv', 'r')
+rdr = csv.reader(f)
+wedo = []
+keungdo = []
+for line in rdr:
+    wedo.append(float(line[9]))
+    keungdo.append(float(line[10]))
+    
 
-# ssdfsdfsdf
+f.close()
+
 # 플라스크 모듈 불러오기
 from flask import Flask, render_template, request
 
@@ -14,7 +26,7 @@ app = Flask(__name__)
 # 뷰함수 -> 화면 표시 함수
 # templates 폴더 아래의 index.html 로 연결
 def hello_world():
-    return render_template('tvtvtv.html')
+    return render_template('tvtvtv.html', wedo=wedo)
 
 @app.route("/abcd/")
 # 뷰함수 -> 화면 표시 함수
@@ -25,8 +37,11 @@ def grape():
 
 @app.route('/map/', methods=['POST'])
 def apple():
+
     maping = request.form['maping']
-    return render_template('jidopage.html', maping=maping)
+    
+   
+    return render_template('jidopage.html', maping=maping, wedo=wedo, keungdo=keungdo)
 
 # 앱 실행
 if __name__ == "__main__":
